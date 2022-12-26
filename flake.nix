5659e5333922569a390acd3f33c74248da56dfc8
Mark Pashmfouroush <mark@markpash.me>
2022-12-26 20:46:57 +0000

    use flake-parts
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- flake.nix
+++ flake.nix
@@ -2,14 +2,13 @@
   description = "nix-playground";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux =
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
-      {
+  outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    flake = { };
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    perSystem = { pkgs, ... }: {
+      packages = {
         cloak = (import ./pkgs/cloak.nix) { inherit pkgs; };
         go-shadowsocks2 = (import ./pkgs/go-shadowsocks2.nix) { inherit pkgs; };
         gost = (import ./pkgs/gost.nix) { inherit pkgs; };
@@ -23,7 +22,7 @@
         tun2socks = (import ./pkgs/tun2socks.nix) { inherit pkgs; };
         v2ray-core = (import ./pkgs/v2ray-core.nix) { inherit pkgs; };
         xray-core = (import ./pkgs/xray-core.nix) { inherit pkgs; };
-        default = nixpkgs.legacyPackages.x86_64-linux.hello;
       };
+    };
   };
 }
